d5d38060d95fa2ed41c6b44c3c914705e2f41563
NaveenSingh <raghuvamshinaveensingh@gmail.com>
2025-04-09 22:57:01 +0530

    Add CardDetails component

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import RegisterForm from './Components/RegisterForm.jsx'
 import App from './App.jsx'
-import CardDetails from './Components/cardDetails.jsx'
+import CardDetails from './Components/CardDetails.jsx'
 import Login from './Components/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
